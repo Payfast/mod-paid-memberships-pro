2627d7b489f3c713ad492209a5e652ce7dc4e9a7
Cate Faull <cate.faull@payfast.co.za>
2017-09-04 15:26:26 +0200

    Remove Copyright

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # paid-memberships-pro-1_8
 
-Copyright (c) 2008 PayFast (Pty) Ltd
-You (being anyone who is not PayFast (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
-Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
-
 INTEGRATION INSTRUCTIONS:
 1. Unzip the module to a temporary location on your computer.
 2. Copy the 'wp-content' folder in the archive to your base WordPress folder
--- wp-content/plugins/paid-memberships-pro/classes/gateways/class.pmprogateway_payfast.php
+++ wp-content/plugins/paid-memberships-pro/classes/gateways/class.pmprogateway_payfast.php
@@ -2,13 +2,7 @@
 /**
  * class.pmprogateway_payfast.php
  *
- *Copyright (c) 2008 PayFast (Pty) Ltd
- * You (being anyone who is not PayFast (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
- * Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
- *
  * @author     Ron Darby - PayFast
- * @copyright  2009-2014 PayFast (Pty) Ltd
- * @license    http://www.opensource.org/licenses/lgpl-license.php LGPL
  */
     require_once(dirname(__FILE__) . "/class.pmprogateway.php");
 	//load classes init method
